7848a182da4e1fe99a117bf4690af2e22e3b23f1
Aurélien Manteaux <amanteaux@coreoz.com>
2022-03-28 13:59:53 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { HttpQueryParam, HttpQueryParamValue, HttpOptions, HttpClient, HttpRequest } from './lib/HttpRequest';
+export { HttpMethod } from './lib/HttpMethod';
---
+++ src/lib/HttpMethod.ts
@@ -0,0 +1,10 @@
+/**
+ * Represent an HTTP method.
+ */
+export const enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH',
+}
