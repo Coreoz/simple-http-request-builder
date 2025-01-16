0a3ffdbc4521092f843fc5ae34af74ffd59261db
Lucas Amiaud <lamiaud@coreoz.com>
2025-01-16 11:51:03 +0100

    add multipart http request

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ export { HttpRequest } from './lib/HttpRequest';
 export type {
   HttpClient, HttpQueryParam, HttpQueryParamValue, HttpOptions,
 } from './lib/HttpRequest';
+export { MultipartHttpRequest } from './lib/MultipartHttpRequest';
+export type {
+  MultipartHttpClient, MultipartHttpOptions,
+} from './lib/MultipartHttpRequest';
 export { HttpMethod } from './lib/HttpMethod';
